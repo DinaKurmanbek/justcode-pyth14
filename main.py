96194c6f99b46b85e3e03a199583b4dc5b7a8d27
DinaKurmanbek <dina.kk.00@gmail.com>
2023-11-21 17:11:56 +0600

    "change main and add file"

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ a = requests.get(url)
 # print(a.status_code)
 binary_img = a.content
 
-print ("Print for test changes")
+print ("Print for test changes ")
 
 with open("test_img.jpg" , "wb") as file:
     file.write(binary_img)
